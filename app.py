86409b3b78abbe9219906007d4784d519c0c83a1
Swapnil Katiyar <swapnil240695@gmail.com>
2026-06-26 00:19:51 +0530

    database setup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from database.db import get_db, init_db, seed_db
 
 app = Flask(__name__)
 
@@ -62,4 +63,7 @@ def delete_expense(id):
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        init_db()
+        seed_db()
     app.run(debug=True, port=5001)
